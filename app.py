bf827a1ed04f8f8d42000819a1c0050d256df129
C Shiva Sai <chipla038@gmail.com>
2026-06-09 13:47:15 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+
+import streamlit as st
+st.title("Credit Risk Prediction App")
+st.write("This is a placeholder Streamlit app.")
+
+# You would add your model loading and prediction logic here
+# For example:
+# import pickle
+# model = pickle.load(open('model.pkl', 'rb'))
+# user_input = st.slider("Person Income", 0, 100000)
+# prediction = model.predict([[user_input, ...]])
+# st.write(f"Prediction: {prediction}")
